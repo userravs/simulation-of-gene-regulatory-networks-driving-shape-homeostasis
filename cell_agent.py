c3ec6077cff0e1d5d7a2d3d124d296c7b9e9645c
Jose Esteban Perez Hidalgo <josees@o7152b-02.o7152b.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-09-04 17:26:01 +0200

    first sketch

---
+++ cell_agent.py
@@ -0,0 +1,135 @@
+####################################################
+import numpy as np
+import matplotlib.pyplot as plt
+
+class cell:
+	# defines whats needed when a new agent (Cell) of this class is created
+	def __init__(self, xPos, yPos):
+		self.xPos = xPos
+		self.yPos = yPos
+		#self.health = health
+	#
+
+	def Sense(self, xPos, yPos):
+		# sense chemicals from the grid
+		SGF_read = grid[self.xPos, self.yPos][1] # grid contains three values on each coordinate: occupation (boolean), SGF level, LGF level
+		LGF_read = grid[self.xPos, self.yPos][2]
+		#reads = [SGF_read, LGF_read]
+		return SGF_read, LGF_read
+	#    
+
+	def GenerateStatus(self, SGF_read, LGF_read):
+		# neural network generates a status based on the reads
+		status = 0
+		r = np.random.random
+		if r > 0.5
+			status = 1
+		return status
+	#
+
+	def Split(self, grid):
+		# check a randomly generated neighbour if occupied
+		r = np.random.ranint(4)
+		# check if spot is occupied
+		if(r = 0)
+			# each case returns the value on grid according to the random number (neighbour)
+			newxPos = self.xPos - 1
+			newyPos = self.yPos
+
+		elif(r = 1)
+			newxPos = self.xPos
+			newyPos = self.yPos + 1
+
+		elif(r = 2)
+			newxPos = self.xPos + 1
+			newyPos = self.yPos
+
+		else
+			newxPos = self.xPos
+			newyPos = self.yPos - 1
+
+		occupation = grid[newxPos, newyPos][0]
+
+		# if the position is free then create a cell there
+		if(occupation = 0)
+			daughterCell = Cell(newxPos, newyPos)
+			grid[newxPos][newyPos][0] = 1
+			return grid
+		else
+			return grid
+	#
+# Cell
+	#def GetPos(self):
+	#	return [self.xPos,self.yPos]
+	#
+
+class environment:
+	def AntGridFigure(fieldSize, maxFoodAmount, nestPosition):    
+		'''
+		This method has to be called before the looping occours, for a better performance.
+		Inspired by:
+		http://bastibe.de/2013-05-30-speeding-up-matplotlib.html
+		'''
+		#=======================================================================
+		# The Figure
+		#======================================================================= 
+		# Grid:	
+		cmapGrid = plt.cm.get_cmap('cool')#('PiYG_r')#('YlOrRd')#('Blues')#('YlOrRd')
+		figname='Environment'					# main title for plot
+		figsizeX =18						# outer plot dimensions
+		figSizeY = 9                
+		figsize=(figsizeX,figSizeY)
+		cellsFigure = plt.figure(figname,figsize)		# declare main (outer) plot
+		cellsSubPlot = cellsFigure.add_subplot(111)     	# add subplot: cell grid
+		cellsSubPlot.set_aspect('equal')			# settings for grid 
+		cellsSubPlot.set_title('Environment',fontsize=32)
+		cellsSubPlot.set_xlabel('x',fontsize=25)
+		cellsSubPlot.set_ylabel('y',fontsize=25)
+		#cellsSubPlot.grid(True,linestyle='-',color='0.75')
+		cellsSubPlot.set_xlim(-0.5, fieldSize+0.5-1)
+		cellsSubPlot.set_ylim(-0.5, fieldSize+0.5-1)     
+		plt.axis('off')
+		#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~		
+		npaAgentForagingPosY = np.array([0])
+		npaAgentForagingPosX = np.array([0])
+		npaFoodAmount = np.zeros((fieldSize,fieldSize))
+		#
+		gridPlot = cellsSubPlot.imshow(npaFoodAmount, interpolation='none', cmap=cmapGrid, vmin=0, vmax=maxFoodAmount)
+		cellsPlotFood, 	= cellsSubPlot.plot(npaAgentForagingPosY, npaAgentForagingPosX, 'p', ms=7, color='orange', label='cells foraging')
+		antPlotHome, 	= cellsSubPlot.plot(npaAgentForagingPosY, npaAgentForagingPosX, 'o', ms=10, color='blue', label='cells returning home')	
+		nestPlot, 		= cellsSubPlot.plot(nestPosition,nestPosition, 'o', ms=30, color='black', label='Nest')	
+		# Legend
+		handles, labels = cellsSubPlot.get_legend_handles_labels()
+		display = (0,1,2)
+
+		
+		# Now add the legend with some customizations.
+		cellsSubPlot.legend(numpoints=1, shadow=True, bbox_to_anchor=(0,0,-0.05, 1), fontsize=25)
+		
+		#=======================================================================
+		# # Create custom artists
+		# #cellsSubPlot.legend(scatterpoints=1, bbox_to_anchor=(0,0,-0.05, 1))
+		# cellsForaging 		= plt.Line2D((0,1),(0,0), color='violet', marker='p')
+		# cellsReturningHome 	= plt.Line2D((0,1),(0,0), color='blue', marker='o')
+		# nest				= plt.Line2D((0,1),(0,0), color='black', marker='o')
+		# #food				= plt.Line2D((0,1),(0,0), color='black', marker='s')
+		# # Create legend from custom artist/label lists
+		# cellsSubPlot.legend([handle for i,handle in enumerate(handles) if i in display]+[cellsForaging,cellsReturningHome,nest],
+		# 					[label for i,label in enumerate(labels) if i in display]+['cells foraging', 'cells returning home', 'Nest'],
+		# 					numpoints=1, bbox_to_anchor=(0,0,-0.05, 1))
+		#=======================================================================
+
+		#
+		img = plt.imshow(np.array([[0,1]]), cmap=cmapGrid)
+		img.set_visible(False)		
+		cbarAmountGrid = plt.colorbar(orientation="vertical")
+		cbarAmountGrid.set_label('Amount of food',size=25)
+		#
+		plt.ion()
+		plt.pause(0.0001)
+		cellsFigure.canvas.draw()
+		plt.ioff()
+		#
+		return cellsSubPlot, cellsFigure, cellsPlotFood, antPlotHome, gridPlot, nestPlot
+	#
+# Environment
---
+++ main.py
@@ -0,0 +1,77 @@
+import time
+import random 
+import numpy as np
+import matplotlib.pyplot as plt
+from cell_agent import Cell		# it is allowed to call from this class because there's an __init__.py file in this directory
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#	PARAMETERS			#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+nLattice = 128 # Lattice Size
+timeSteps = 1000 # Number of simulation time steps
+p = 0.5 # Probability of splitting for any time step
+# f = 1 # Lightning probability.
+cellGrid = np.zeros([nLattice+2,nLattice+2]) # Initialize empty forest
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#	INITIALIZATION			#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+fig = plt.figure()
+
+# initialise mother cell
+mother_cell = cell()
+SGF_read = 0
+LGF_read = 0
+cellList = []
+tmpCellList
+time = 0
+print('Time running...')
+
+while time < timeSteps:
+##    print('time step #'+str(time))
+    #forestLattice = UpdateForest(forestLattice,nLattice,p)
+    #time += 1
+    #ignitedTrees = []
+    #fireAlert = 0 # turn off the fire alert
+    
+    ## A random lightning strike initiates a fire
+    #r = np.random.random()
+    #if r < f:
+        #rXPos = random.randint(1,nLattice)
+        #rYPos = random.randint(1,nLattice)
+        #if forestLattice[rYPos,rXPos] == 1:
+            #forestLattice[rYPos,rXPos] = -1
+            #ignitedTrees.append([rYPos,rXPos])
+            #fireCounter += 1 # variable n
+            #fireAlert = 1 # there's a fire in this timestep
+    
+    ## if there's a fire in this time step...                
+    #if fireAlert == 1: 
+        #forestLattice = FireSpread(forestLattice,ignitedTrees)
+##        burnedTrees = len(ignitedTrees)
+##        treeDensity.append(burnedTrees/(nLattice**2))
+    cellList = []
+
+# first update cell status
+    status = GenerateStatus(SGF_read, LGF_read)
+
+# according to cell status perform action: split or stay quiet
+    if(status == 1)
+	newGrid = Split(cellGrid)
+
+# plot grid
+
+# repeat
+    
+
+## IMSHOW PLOT, must be in the fire spread while            
+    plt.clf()
+    plt.imshow(cellGrid, origin='lower', cmap='RdYlGn', interpolation='none', vmin =-1, vmax = 1)
+    plt.colorbar()
+    fig.canvas.draw()
+    time.sleep(0.51) 
+#    plt.savefig('fire_spread-p'+str(p)+'-f'+str(f)+'-timeStep'+str(time)+'.png', bbox_inches='tight')
+
+
+
+print(str(timeSteps)+' time steps complete')
