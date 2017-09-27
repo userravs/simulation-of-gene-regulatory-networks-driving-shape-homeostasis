c8bca1cfdcea4ec2cb8fdc1a4ed8b03c9ca0ddf3
Esteban Pérez <j0sees>
2017-09-27 17:29:53 +0200

    initial code for GA

---
+++ main_GA.py
@@ -0,0 +1,62 @@
+import time
+import random 
+import numpy as np
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#       PARAMETERS                 #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+popSize = 30                                                # Population size
+nGenes = 40                                                 # Number of genes
+crossoverProb = 0.8                                         # Crossover probability
+mutationProb = 0.025                                        # Mutation probability
+tournamentSelParam = 0.75                                   # Tournament selection parameter
+varRange = 3.0                                              # variable range
+nOfGenerations = 100
+fitness = np.zeros([popSize])                               # fitness array
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#       INITIALISATION             #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+population = InitializePopulation(popSize, numberOfGenes)   # call initialization function, a random set of chromosomes is generated
+
+for iGen in range(nOfGenerations):
+    maximumFitness = 0.0 # Assumes non-negative fitness values!
+    xBest = zeros(1,2) # [0 0]
+    bestIndividualIndex = 0
+
+    # chromosomes get decoded and evaluated
+    for ix in range(0,popSize,2):                               # only every other individual gets considered
+        chromosome = population[i,:]
+        x = DecodeChromosome(chromosome, variableRange)
+        fitness[i] = EvaluateIndividual(x)
+        if fitness[i] > maximumFitness:
+            maximumFitness = fitness[i]
+            bestIndividualIndex = i
+            xBest = x
+    tempPopulation = np.array(population)
+
+    for i in range(popSize):
+        i1 = TournamentSelect(fitness,tournamentSelParam)
+        i2 = TournamentSelect(fitness,tournamentSelParam)
+        chromosome1 = population[i1,:]
+        chromosome2 = population[i2,:]
+        tempPopulation[i,:] = chromosome1
+        tempPopulation[i+1,:] = chromosome2
+
+        r = rand
+        if r < crossProb:
+            newChromosomePair = Cross(chromosome1,chromosome2)
+            tempPopulation[i,:] = newChromosomePair[1,:]
+            tempPopulation[i+1,:] = newChromosomePair[2,:]
+        else:
+            tempPopulation[i,:] = chromosome1
+            tempPopulation[i+1,:] = chromosome2
+    # Loop over population
+
+    for i in range(popSize):
+        originalChromosome = tempPopulation[i,:]
+        mutatedChromosome = Mutate(originalChromosome,mutProb)
+        tempPopulation[i,:] = mutatedChromosome
+
+    tempPopulation[1,:] = population[bestIndividualIndex,:]
+    population = np.array(tempPopulation)
---
+++ tools_GA.py
@@ -0,0 +1,94 @@
+import time
+import random 
+import numpy as np
+
+# this doesn't have to be a function, its just one line...
+def InitializePopulation(populationSize, numberOfGenes):
+    population = np.random.randint(0,2,size=(populationSize, numberOfGenes))
+    #population = np.zeros([populationSize, numberOfGenes]);
+    #for ix in range(populationSize):
+        #for jx in range(numberOfGenes):
+            #s = np.random.random()
+            #if s < 0.5:
+                #population[ix,jx] = 0
+            #else:
+                #population[ix,jx] = 1
+    return population
+# InitializePopulation
+
+# translate the chromosome into actual variables 
+def DecodeChromosome(chromosome,varRange):
+    nGenes = len(chromosome)
+    nHalf = int(nGenes/2.)      # WARNING: assumes number of genes is even
+    x1 = 0.0
+    for jx in range(nHalf):
+        x1 = x1 + chromosome[j]*2^(-j)
+    x1 = -varRange + 2*varRange*x1/(1. - 2.**(-nHalf))
+    x2 = 0.0
+    for jx in range(nHalf):
+        x2 = x2 + chromosome[j+nHalf]*2^(-j)
+    x2 = -varRange + 2*varRange*x2/(1. - 2.**(-nHalf))
+# DecodeChromosome
+
+# Evaluate variable using the fitness function
+def EvaluateIndividual(x):
+    totSum = 0
+    for ix in range(border):
+        for jx in range(border):
+            totSum += DiracDelta(ix,jx)
+    fitness = 1. - (1./(lSize**2))*totSum
+    return fitness
+# EvaluateIndividual
+
+# Dirac's delta function
+def DiracDelta(a, b):
+    if a == b:
+        return 0
+    else: 
+        return 1
+# DiracDelta
+
+# Tournament selection, tournament size 2
+def TournamentSelect(fitness,tournamentSelParam)
+    populationSize = len(fitness)
+    iTmp1 = 1 + int(np.random.random()*populationSize)
+    iTmp2 = 1 + int(np.random.random()*populationSize)
+    r = np.random.random()
+    if r < tournamentSelParam:
+        if fitness[iTmp1] > fitness[iTmp2]:
+            iSelected = iTmp1
+        else:
+            iSelected = iTmp2
+    else:
+        if fitness[iTmp1] > fitness[iTmp2]:
+            iSelected = iTmp2
+        else:
+            iSelected = iTmp1
+    return iSelected
+# TournamentSelect
+
+# Crossover
+def Cross(chromosome1,chromosome2):
+    someOtherNumber,nGenes = chromosome1.shape                # Both chromosomes must have the same length!
+    crossoverPoint = 1 + int(np.random.random()*(nGenes-1))
+    newChromosomePair = np.zeros([2,nGenes])
+    for j in range(nGenes):
+        if j < crossoverPoint:
+            newChromosomePair[0,j] = chromosome1[j]
+            newChromosomePair[1,j] = chromosome2[j]
+        else:
+            newChromosomePair[0,j] = chromosome2[j]
+            newChromosomePair[1,j] = chromosome1[j]
+    return newChromosomePair
+# Cross
+
+# Mutation
+def Mutate(chromosome,mutProb):
+    someOtherNumber,nGenes = chromosome1.shape
+    mutatedChromosome = np.array(chromosome)
+    for j in range(nGenes):
+        r = np.random.random()
+        if r < mutProb:
+            mutatedChromosome[j] = 1-chromosome[j]
+    return mutatedChromosome
+# Mutate
