4086850ff1e5574f35af21110446fb3ab79924be
Esteban Pérez <j0sees>
2017-10-02 15:35:20 +0200

    writing selection code: tournament, elitism, crossover and mutation

--- main_GA.py
+++ main_GA.py
@@ -7,70 +7,82 @@ import numpy as np
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 popSize = 30                                                # Population size
 nNodes = 25
-nGenes = nNodes**2                                                 # Number of genes
+nGenes = nNodes**2                                          # Number of genes
 crossoverProb = 0.8                                         # Crossover probability
 mutationProb = 0.025                                        # Mutation probability
 tournamentSelParam = 0.75                                   # Tournament selection parameter
-eliteNum = 6                                              # number of elite solutions to carry to next generation
+eliteNum = 6                                                # number of elite solutions to carry to next generation
 nOfGenerations = 200
-fitness = np.zeros([popSize,2])                               # fitness array
-posList = []
+#fitness = np.zeros([popSize,2])                            # fitness array
 eliteIndividuals = []
-dtype = [('fitness',float),('position',int)]
+dtype = [('fitnessValue',float),('position',int)]           # format for fitness array, for an easier sort
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #       INITIALISATION             #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #population = InitializePopulation(popSize, numberOfGenes)  # call initialization function, a random set of chromosomes is generated
-population = np.random.random(size=(popSize, nGenes))
+population = np.random.random(size = (popSize, nGenes))
 
 
 for iGen in range(nOfGenerations):
-    maxFitness = 0. # Assumes non-negative fitness values!
-    fitness = np.array(('',-1),dtype = dtype)
+    #maxFitness = 0. # Assumes non-negative fitness values!
+    fitness = np.zeros(popsize, dtype = dtype)              # structured array which will contain a fitness value for each individual
     #wBest = np.zeros(nGenes) # [0 0]
-    bestIndividualIndex = 0
+    #bestIndividualIndex = 0
 
     # chromosomes get decoded and evaluated
     for ix in range(popSize):
-        posList[ix] = ix
-        chromosome = np.array(population[ix,:])                 # loop through all chromosomes
-        wMatrix = -1 + 2*chromosome.reshape(nNodes,nNodes)      # decode chromosome,i.e., transform into matrix
-        np.append(fitness, EvaluateIndividual(wMatrix),ix)          # get chromosome fitness
-        fitness[ix,1] =                                         # store position in population matrix
+        chromosome = np.array(population[ix,:])             # loop through all chromosomes
+        wMatrix = -1 + 2*chromosome.reshape(nNodes,nNodes)  # decode chromosome, i.e., transform into matrix
+        fitness[ix][0] = EvaluateIndividual(wMatrix)        # get chromosome fitness
+        fitness[ix][1] = ix                                 # store position in population matrix
         
-        #if fitness[ix] > maximumFitness:                       # get index of best chromosome
+        #if fitness[ix] > maximumFitness:                   # get index of best chromosome
             #maximumFitness = fitness[ix]
             #bestIndividualIndex = ix
-    fitness = np.sort(fitness, order = 'fitness')
-    wBest = np.array(population[bestIndividualIndex,:])         # store the best chromosome
+    fitness.sort(order = 'fitnessValue')                    # sort array according to fitness value. Less fit to most fit
+    #wBest = np.array(population[bestIndividualIndex,:])     # store the best chromosome
     
     tempPopulation = np.array(population)
 
-    for i in range(0,popSize,4):                                # only every other individual gets considered
-        i1 = TournamentSelect(fitness,tournamentSelParam)
-        i2 = TournamentSelect(fitness,tournamentSelParam)
-        chromosome1 = population[i1,:]
-        chromosome2 = population[i2,:]
-        tempPopulation[i,:] = chromosome1
-        tempPopulation[i+1,:] = chromosome2
+    iElit = 0
 
-        r = np.random.random()
-        if r < crossProb:
-            newChromosomePair = Cross(chromosome1,chromosome2)
-            tempPopulation[i,:] = newChromosomePair[1,:]
-            tempPopulation[i+1,:] = newChromosomePair[2,:]
-        else:
-            tempPopulation[i,:] = chromosome1
-            tempPopulation[i+1,:] = chromosome2
-    # Loop over population
+    while iElit < elitNum:
+        index = fitness[popSize - iElit][1]
+        tempPopulation[iElit] = np.array(population[index,:])
+        del 
+        iElit += 1
+        
 
-    for i in range(popSize):
-        originalChromosome = tempPopulation[i,:]
-        mutatedChromosome = Mutate(originalChromosome,mutProb)
-        tempPopulation[i,:] = mutatedChromosome
+    #for iInd in range(len(fitness)-1,-1,-1):
+        #if iPos < 6:
+            #index = fitness[iInd][1]
+            #tempPopulation[iPos] = np.array(population[index,:])
+            #iPos += 1
+    #for i in range(0,popSize,4):                                # only every other individual gets considered
+        #i1 = TournamentSelect(fitness,tournamentSelParam)
+        #i2 = TournamentSelect(fitness,tournamentSelParam)
+        #chromosome1 = population[i1,:]
+        #chromosome2 = population[i2,:]
+        #tempPopulation[i,:] = chromosome1
+        #tempPopulation[i+1,:] = chromosome2
 
-    tempPopulation[1,:] = population[bestIndividualIndex,:]
+        #r = np.random.random()
+        #if r < crossProb:
+            #newChromosomePair = Cross(chromosome1,chromosome2)
+            #tempPopulation[i,:] = newChromosomePair[1,:]
+            #tempPopulation[i+1,:] = newChromosomePair[2,:]
+        #else:
+            #tempPopulation[i,:] = chromosome1
+            #tempPopulation[i+1,:] = chromosome2
+    ## Loop over population
+
+    #for i in range(popSize):
+        #originalChromosome = tempPopulation[i,:]
+        #mutatedChromosome = Mutate(originalChromosome,mutProb)
+        #tempPopulation[i,:] = mutatedChromosome
+
+    #tempPopulation[1,:] = population[bestIndividualIndex,:]
     population = np.array(tempPopulation)
 
 # Loop over generations
